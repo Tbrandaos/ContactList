de6b79ab654c62d56dc94fa1e169b21f737c2c68
Thiago Brandão <thiago_brandaos@outlook.com>
2022-10-07 19:26:16 -0300

    remove unused front folder

---
+++ Front/contact-list/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonTable from './pages/PersonTable';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonTable></PersonTable>
+    </div>
+  );
+}
+
+export default App;
