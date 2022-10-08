f39627008544247629a0311a3351766c4370868c
Thiago Brandão <thiago_brandaos@outlook.com>
2022-10-08 15:33:14 -0300

    remove unused icons

--- Front/contact-list/src/App.tsx
+++ Front/contact-list/src/App.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import People from './pages/People';
 
